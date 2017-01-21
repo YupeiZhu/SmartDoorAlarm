11ae000a395b430a6c2d54688ff0891ba0945a55
Yupei Zhu <1002500403@qq.com>
2017-01-20 20:26:15 -0800

    list done

--- app/src/main/java/hackthon/smartdooralarm/MainActivity.java
+++ app/src/main/java/hackthon/smartdooralarm/MainActivity.java
@@ -1,13 +1,58 @@
 package hackthon.smartdooralarm;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView list;
+    private String[] web = {"a"};
+    private Integer[] imageId = {R.mipmap.ic_launcher};
+    private static final int NONSELECTED_STATE = -1;
+    private TextView tvSum;
+    private LinearLayout linearLayout;
+    private Button btCancel, btDelete;
+    private boolean isSelecting = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CustomList adapter = new CustomList(MainActivity.this, web, imageId);
+        list = (ListView)findViewById(R.id.picList);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
+                Dialog builder = new Dialog(MainActivity.this);
+                builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                builder.getWindow().setBackgroundDrawable(
+                        new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        //nothing;
+                    }
+                });
+
+                ImageView imageView = new ImageView(MainActivity.super.getApplicationContext());
+                builder.addContentView(imageView, new RelativeLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+                builder.show();
+
+            }
+        });
     }
 }
